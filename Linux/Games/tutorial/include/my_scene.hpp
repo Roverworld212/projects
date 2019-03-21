de6db1bf131e962ba7e5af9774fb732393c74d36
Roverworld212 <32963198+Roverworld212@users.noreply.github.com>
2019-03-20 19:20:12 -0500

    Create my_scene.hpp

---
+++ Linux/Games/tutorial/include/my_scene.hpp
@@ -0,0 +1,20 @@
+#ifndef MY_SCENE_HPP
+#define MY_SCENE_HPP
+
+#include <SFML/Graphics.hpp>
+
+#include "gs2d/scene.hpp"
+
+class MyScene : public gs::Scene {
+
+private:
+   sf::CircleShape circle;
+
+public:
+   void start();
+   void update();
+   void draw_entities();
+
+};
+
+#endif
