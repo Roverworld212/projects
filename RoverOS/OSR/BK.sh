c50730830c26ac6375428227ae7676654d83ff38
Roverworld212 <32963198+Roverworld212@users.noreply.github.com>
2019-04-02 08:31:42 -0500

    Update BK.sh

--- RoverOS/OSR/BK.sh
+++ RoverOS/OSR/BK.sh
@@ -1,1 +1,6 @@
-build
+echo "Building boot.asm";
+nasm -f -aout -o ./kernel/boot.asm ./OSR/boot.o;
+echo "Building kernel";
+gcc ./kernel/kernel.c -o ./OSR/kernel.o;
+echo "Linking files";
+Obviously not finished I will finish it later
