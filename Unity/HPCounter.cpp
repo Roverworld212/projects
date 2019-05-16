5296af92fc40eeac7614b92b450417818fc76693
Roverworld212 <32963198+Roverworld212@users.noreply.github.com>
2019-05-16 11:28:28 -0500

    Create HPCounter.cpp

---
+++ Unity/HPCounter.cpp
@@ -0,0 +1,14 @@
+//blah blah blah using tags
+//My spaces may cause the code to fail btw
+//The spacebar is broken
+private int hitPoints = 100;
+private int maxHP = 100;
+
+void Start(){
+}
+
+void Update(){
+}
+
+void takeDamage(int dmg){
+}
