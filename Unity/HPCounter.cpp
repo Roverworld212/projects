3d9c8b0ca9058004e60811c4002cdd59f8df6a1a
Roverworld212 <32963198+Roverworld212@users.noreply.github.com>
2019-05-16 11:42:21 -0500

    Update HPCounter.cpp

--- Unity/HPCounter.cpp
+++ Unity/HPCounter.cpp
@@ -1,14 +1,50 @@
-//blah blah blah using tags
+//blah blah blah unity using tags go here
 //My spaces may cause the code to fail btw
-//The spacebar is broken
-private int hitPoints = 100;
-private int maxHP = 100;
+//My spacebar is broken
 
-void Start(){
-}
+public class BC : Monobehavior {
+  //Vars
+  private int hitPoints = 100;//CurrentHP
+  private int maxHP = 100;//Max HP allowed
+  private deathNum = 0;//Customize the death limit
+  
+  void Start(){
+  }
 
-void Update(){
-}
+  void Update(){
+    deathCheck();
+  }
+  
+  void deathCheck(){
+    if(hitPoints <= deathNum){
+      //DIE
+    }
+  }
 
-void takeDamage(int dmg){
+  void takeDamage(int dmg){
+    deathCheck();
+    if(hitPoints < dmg){
+      hitPoints = 0;
+    }
+    else{
+      hitPoints = hitPoints - dmg
+    }
+    deathCheck();
+  }
+  
+  void takeHealth(int hp){
+    deathCheck();
+    int atc = hitPoints + hp;//This may be unnecessary to do
+    if(atc > maxHP){
+      hitPoints = maxHP;
+    }
+    else{
+      hitPoints = hitPoints + hp;
+    }
+  }
+  
+  void printOutHP(){
+    Debug.Log(hitPoints);
+  }
+  
 }
