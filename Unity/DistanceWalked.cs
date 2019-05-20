0bf07cf8889deb15f6647b474fbdb1b5c5549945
Roverworld212 <32963198+Roverworld212@users.noreply.github.com>
2019-05-20 12:20:26 -0500

    Update DistanceWalked.cs

--- Unity/DistanceWalked.cs
+++ Unity/DistanceWalked.cs
@@ -1,1 +1,25 @@
-//This one will be a bit harded to do
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//There are always some sort of errors in my code
+//Typing this on a restricted school computer doesn't let me test it
+public class DistanceWalked : MonoBehaviour
+{
+float time = 0f;
+float distanceWalked = 0f;
+
+void Update(){
+while(/*W is held*/){//Get the seconds W is held
+timer += Time.deltaTime;
+int seconds = timer % 60;
+}
+Calculate(seconds);//Calculate the distance walked
+}
+
+void Calculate(int s){
+while(s >= 60){//for every 60 seconds add 0.05 to the distance
+s = s - 60;//Subtract 60
+distanceWalked = distanceWalked + 0.05;//Add to the distance  
+}
+}
+}
