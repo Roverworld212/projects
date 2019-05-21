936d5fcccbef4fbb3c54b176b36467779e4baac1
Roverworld212 <32963198+Roverworld212@users.noreply.github.com>
2019-05-21 12:21:31 -0500

    Update pausemenu.cs

--- Unity/pausemenu.cs
+++ Unity/pausemenu.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DistanceWalked : MonoBehaviour
+public class pausemenu : MonoBehaviour
 {
-
-
-
-
+bool isPaused = false;
+void FixedUpdate(){
+if(/*esc is pressed*/){
+isPaused != isPaused;
+if(isPaused == true){
+//Open menu
+}
+else{
+//Close menu
+}
+}
+}
 
 }
