7a9e218e0a411792273a16a8fc25adf12a48ede5
Roverworld212 <32963198+Roverworld212@users.noreply.github.com>
2019-08-08 14:27:00 -0500

    Add files via upload

---
+++ rOSrewrite/run.sh
@@ -0,0 +1,13 @@
+cd <PATH_TO_/rOSrewrite>
+as --32 ./boot.s -o ./COMPF/boot.o
+
+gcc -w -m32 -c ./kernel.c -o ./COMPF/kernel.o -I./Include -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+
+ld -m elf_i386 -T linker.ld ./COMPF/kernel.o ./COMPF/boot.o -o ./rOS.bin -nostdlib
+
+grub-file --is-x86-multiboot rOS.bin
+
+mkdir -p ./isodir/boot/grub
+cp ./rOS.bin ./isodir/boot/rOS.bin
+cp ./grub.cfg ./isodir/boot/grub/grub.cfg
+grub-mkrescue -o ./rOS.iso ./isodir
