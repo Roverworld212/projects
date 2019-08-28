c9e4773163b8b549b41180625e64bf437a08ba51
Roverworld212 <32963198+Roverworld212@users.noreply.github.com>
2019-08-28 14:37:55 -0500

    Update MTUTILS.h

--- rOSrewrite/Include/MTUTILS.h
+++ rOSrewrite/Include/MTUTILS.h
@@ -5,12 +5,15 @@ Required .h Files: (UTILS.h)
 
 #define PID int
 
-int apid[99];//100 max programs
+int apid[99];//100 max programs including the kernel
 
 int assign_pid(){//Returns an avalible PID
   PID i = rand(10, 30);//Starts with rand number for PID
-  int a = 0;//Used for while statment
+  int a = 1;//Used for while statment
   while(apid[99]){//Checks for conflicting PIDS
+    if(a == 0){
+      a++;
+    }
     if(i == apid[a]){
       i = rand(a, 15);//If a PID is unavalible it regenerates a new PId using i
     }
