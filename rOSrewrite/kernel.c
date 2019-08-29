6b3b024146592e42d67671e5c5ac4edcf4ede60e
Roverworld212 <32963198+Roverworld212@users.noreply.github.com>
2019-08-29 14:23:12 -0500

    Added more boot info

--- rOSrewrite/kernel.c
+++ rOSrewrite/kernel.c
@@ -23,6 +23,12 @@ void kernel_main() {
 	apid[0] = 0;//Kernel has PID 0
 	init_vga(WHITE, BLACK);
 	pstring("VGA Initialized", 1);
+	pstring("VGA BUFFER: ",0);
+	pstring(&vgabuffer, 1);
+	pstring("CURRENT INDEX: ", 0);
+	pint(vga_index, 1);
+	pstring("USING KEYBOARD PORT: ", 0);
+	pstring(KEYBOARD_PORT, 1);
 	tkbd();
 	pstring("Loading Terminal", 1);
 	int i = termstart();
