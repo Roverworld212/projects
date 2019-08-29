875860bea19a81c74c85e5b811e197ef5c5b2a27
Roverworld212 <32963198+Roverworld212@users.noreply.github.com>
2019-08-29 14:33:28 -0500

    Update MTUTILS.h

--- rOSrewrite/Include/MTUTILS.h
+++ rOSrewrite/Include/MTUTILS.h
@@ -11,7 +11,7 @@ int assign_pid(){//Returns an avalible PID
   PID i = rand(10, 30);//Starts with rand number for PID
   int a = 1;//Used for while statment
   while(apid[99]){//Checks for conflicting PIDS
-    if(a == 0){
+    if(a == 0){//Cannot overwrite the kernels PID
       a++;
     }
     if(i == apid[a]){
