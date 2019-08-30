7a522ab9e56c90e8bf9231e065a2c5a19fa98f9c
Roverworld212 <32963198+Roverworld212@users.noreply.github.com>
2019-08-30 14:26:53 -0500

    Outline of functions

--- rOSrewrite/Include/FUTILS.h
+++ rOSrewrite/Include/FUTILS.h
@@ -1,3 +1,15 @@
 /*
 Utils for file management, editing, and creation
 */
+
+void fwrite(){//Write to a file
+}
+
+void fread(){//Read from a file
+}
+
+void fopen(){//Opens a file for the other functions
+}
+
+void fclose(){//Closes a file
+}
